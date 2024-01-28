7720a7a1afabee40eee7b8627362b4330d46fea6
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-28 19:37:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 let arr = [1, 2, 3, 4, 5];
 function incrementArray(arr) {
-  //your code here
+  let newArr = [];
+	for(let i=0;i< arr.length;i++){
+		newArr[i] = ++arr[i];
+	}
+	return newArr;
 }
 
 alert(incrementArray(arr));
